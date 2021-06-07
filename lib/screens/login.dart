118aae3bf73a8752ada4fb49444d3d0a4493e73e
itsmushi <jmushi175@gmail.com>
2021-06-07 08:17:04 +0300

    login page modified

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -1,24 +1,88 @@
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
   final _url = "https://osmmas.herokuapp.com/getstarted/forgot-password.php";
+
+  final _form = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Osmmas"),
       ),
-      body: Card(
-        elevation: 8,
-        child: Column(
-          children: [
-            Text("Login"),
-            Divider(),
-           
-            FlatButton(onPressed: _launchURL, child: Text("I forgot my password"))
-          ],
-        ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          Card(
+            elevation: 8,
+            child: Container(
+              padding: EdgeInsets.symmetric(horizontal: 20),
+              height: 400,
+              child: SingleChildScrollView(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Container(
+                      margin: EdgeInsets.symmetric(vertical: 16),
+                      child: Text("Login",
+                          style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+                    ),
+                    Divider(),
+                    Form(
+                      key: _form,
+                      child: Container(
+                        height: 500,
+                        width: 400,
+                        child: ListView(
+                          children: [
+                            Container(
+                              width: 200,
+                              child: TextFormField(
+                                decoration:
+                                    InputDecoration(hintText: "Email/Username"),
+                                textInputAction: TextInputAction.next,
+                              ),
+                            ),
+                            
+                             
+                              TextFormField(
+                                obscureText: true,
+                                decoration: InputDecoration(hintText: "Password"),
+                                textInputAction: TextInputAction.next,
+                              ),
+                           
+                            Container(
+                              margin: EdgeInsets.symmetric(vertical: 10),
+                              child: RaisedButton(
+                                onPressed: () {},
+                                child: Text("Login"),
+                              ),
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.end,
+                              children: [
+                                FlatButton(
+                                    onPressed: _launchURL,
+                                    child: Text("I forgot my password")),
+                              ],
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                    
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
